44d5d94c4a580221e3578c4b3cc90d8529be1155
Xlxlrdyyy <solo.vlad55555@icloud.com>
2024-12-13 11:14:54 +0000

    сделал

--- task7.py
+++ task7.py
@@ -1,21 +1,19 @@
 def find_max_duplicate(arr):
-    
+
     arr.sort()
-    
-    
+
     count = {}
-    
-for num in arr:
+
+    for num in arr:
         count[num] = count.get(num, 0) + 1
-            
-   
+
     max_duplicate = None
-for num, cnt in count.items():
+    for num, cnt in count.items():
         if cnt > 1:
             if max_duplicate is None or num > max_duplicate:
                 max_duplicate = num
-                
-        return max_duplicate
+
+    return max_duplicate
 
 
 arr = [3, 5, 1, 3, 7, 8, 5, 2, 1]
